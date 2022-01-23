3c37654f0820da6d35844727a31c2083ef453ba0
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2022-01-23 09:55:59 +0800

    added navbar

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import NavBar from '../components/NavBar';
 
 export default function Home() {
-    return <div>Hello World!</div>;
+    return (
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <main className="container"></main>
+        </>
+    );
 }
