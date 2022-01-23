abecd586dea7981853770dab9cd2f3943c1e07bc
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2022-01-23 09:56:14 +0800

    Create db.js

---
+++ utils/db.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: process.env.DB_URL,
+    port: process.env.DB_PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.SCHEMA,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+
+console.log('connected!');
+
+async function executeQuery(query) {
+    return new Promise((resolve, reject) => {
+        pool.query(query, function (err, rows, fields) {
+            if (err) return reject(err);
+            resolve([rows, fields]);
+        });
+    });
+}
+
+module.exports = executeQuery;
