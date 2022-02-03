e4a765d32924a45ff54c21b34023b3e61a584ecf
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2022-02-03 10:51:17 +0800

    env changes

--- pages/api/movies/index.js
+++ pages/api/movies/index.js
@@ -1,14 +1,18 @@
 import query from '/utils/query';
 
 async function getAllMovieData(req, res) {
-    const data = await query(`
+    try {
+        const data = await query(`
     select movies.id as id, movies.name as title, movies.rank as rating, movies.year
     from movies 
     where movies.rank is not null
     `);
 
-    const movies = data[0];
-    res.json(movies);
+        const movies = data[0];
+        res.json(movies);
+    } catch (err) {
+        res.json({ msg: err });
+    }
 }
 
 export default getAllMovieData;
--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql2');
-const pool = mysql.createPool({
-    host: process.env.DB_URL,
+const DB_CONFIG = {
     port: process.env.DB_PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
@@ -8,17 +7,41 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 100,
     queueLimit: 0,
+};
+
+const pool1 = mysql.createPool({
+    host: process.env.DB_URL1,
+    ...DB_CONFIG,
+});
+
+const pool2 = mysql.createPool({
+    host: process.env.DB_URL2,
+    ...DB_CONFIG,
+});
+
+const pool3 = mysql.createPool({
+    host: process.env.DB_URL3,
+    ...DB_CONFIG,
 });
 
 console.log('connected to database!');
 
 async function executeQuery(query) {
     return new Promise((resolve, reject) => {
-        pool.query(query, function (err, rows, fields) {
+        pool2.query(query, function (err, rows, fields) {
             if (err) return reject(err);
             resolve([rows, fields]);
         });
     });
 }
+
+(async function () {
+    const data = await executeQuery('select * from movie_change_events;');
+    console.log(data[0][0].event_timestamp);
+    console.log(new Date(data[0][0].event_timestamp).getTime());
+    console.log(new Date(data[0][1].event_timestamp).getTime());
+    console.log(new Date(data[0][2].event_timestamp).getTime());
+    console.log(new Date(data[0][3].event_timestamp).getTime());
+})();
 
 module.exports = executeQuery;
